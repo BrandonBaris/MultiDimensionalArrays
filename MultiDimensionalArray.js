2c45b045230250afa7c3ef05b5cceb2702f479d7
Brandon Baris <brandonbaris@gmail.com>
2015-01-21 11:23:10 -1000

    generate_1d up

--- MultiDimensionalArray.js
+++ MultiDimensionalArray.js
@@ -1,16 +1,45 @@
 module.exports = {
-  generator: function(){
+  generate_1d : function(somevalue)
+  {
+    var tmpArr = [];
+    var randomNumber;
+
+    if (somevalue > 3)
+      {
+        for(i =0; i < somevalue; i++)
+          {
+            randomNumber = Math.floor(Math.random() *2);
+            tmpArr.push(randomNumber);
+          }
+        return tmpArr;
+      }
+
+    if (somevalue === 'potato')
+      {
+        return tmpArr;
+      }
+
+    for(i =0; i < 3; i++)
+      {
+        randomNumber = Math.floor(Math.random() *2);
+        tmpArr.push(randomNumber);
+      }
+    return tmpArr;
+},
+  
+  generate_2d : function(){
     var tmpArr = [];
 
     for(var i =0; i < 3; i++){
       var innerArray = [];
-      for(var j=0;j<3;j++){
+      //for(var j=0;j<3;j++){
         var randomNumber = Math.floor(Math.random() *2);
-        innerArray.push(randomNumber);
+        tmpArr.push(randomNumber);
 
       }
-    }
-    tmpArr.push(innerArray);
+   // }
+    //tmpArr.push(innerArray);
+     return tmpArr;
   }
-      return tmpArr;
+     
 };
--- test/test-generator.js
+++ test/test-generator.js
@@ -3,20 +3,58 @@ var expect = chai.expect;
 var should = chai.should();
 var mda = require('./../MultiDimensionalArray.js');
 
-describe('something', function() {
+/*describe('something', function() {
   it('should do something', function() {
     var something = 'some value';
     expect(something).to.be.a('string');
     something.should.equal('some value');
   });
+});*/
+
+// GENERATOR 1
+
+describe('The generate_1d function should exist', function()
+  {
+    it('and it should be a function huehuehue', function() 
+      {
+        expect(mda.generate_1d).to.exist();
+        expect(mda.generate_1d).to.be.an('function');
+      });
+  });
+
+describe('calling with no arguments', function()
+  {
+    it('should return an array with 3 random boolean values in it', function() 
+      {
+        expect(mda.generate_1d()).to.be.an('array');
+        expect(mda.generate_1d()).to.have.length(3);
+      });
+  });
+
+describe('calling with one argument 4', function(){
+  it('should return an array with 4 random boolean values in it', function() 
+  {
+    expect(mda.generate_1d(4)).to.be.an('array');
+    expect(mda.generate_1d(4)).to.have.length(4);
+  });
 });
 
-describe('calling with no arguments', function(){
+describe('calling with an argument "potato"', function(){
+  it('should return an empty array', function() 
+  {
+    expect(mda.generate_1d('potato')).to.be.an('array');
+    expect(mda.generate_1d('potato')).to.be.empty();
+  });
+});
+
+// GENERATOR 2
+
+/*describe('calling with no arguments', function(){
   it('should return an array with 3 random boolean values in it', function() {
 //    var values = [[1,1,1],[1,1,1],[1,1,1]];
-    var values = mda.generator();
+    var values = mda.generate_1d();
     expect(values).to.be.an('array');
-    expect(result).to.have.length(3);
+    expect(values).to.have.length(3);
 
     result.forEach(function (innerArray){
       innerArray.forEach(function (innerValue){
@@ -26,14 +64,38 @@ describe('calling with no arguments', function(){
       });
     });
   });
-});
+});*/
 
-/*describe('this shit', function(){
-  it('will do this', function() {
-    var foo = {bar: 'baz'};
-    //expect(foo).to.deep.equal({ bar: 'baz' });
-    expect({ foo: { bar: { baz: 'quux' } } }).to.have.deep.property('foo.bar.baz', 'quux');
+describe('The function generate_2d should exist', function()
+  {
+    it('should be a function', function() 
+      {
+        expect(mda.generate_2d).to.exist();
+        expect(mda.generate_2d).to.be.an('function');
+      });
+  });
+
+describe('calling with no arguments', function()
+  {
+    it('should return an array with 3 random boolean values in it', function() 
+      {
+        expect(mda.generate_1d()).to.be.an('array');
+        expect(mda.generate_1d()).to.have.length(3);
+      });
+  });
+
+describe('calling with one argument 4', function(){
+  it('should return an array with 4 random boolean values in it', function() 
+  {
+    expect(mda.generate_1d(4)).to.be.an('array');
+    expect(mda.generate_1d(4)).to.have.length(4);
   });
 });
-*/
 
+describe('calling with an argument "potato"', function(){
+  it('should return an empty array', function() 
+  {
+    expect(mda.generate_1d('potato')).to.be.an('array');
+    expect(mda.generate_1d('potato')).to.be.empty();
+  });
+});
