cf693e465b62504a0b7ab21b8e761bf3f555f33b
Brandon Baris <brandonbaris@gmail.com>
2015-01-21 19:52:01 -1000

    redid testing variables in test-generator

--- MultiDimensionalArray.js
+++ MultiDimensionalArray.js
@@ -3,11 +3,13 @@ module.exports = {
   {
     var tmpArr = [];
     var randomNumber;
+    var i;
 
     /* tests if the value is not a number (eg. string) and if it's 
     not an undefined number (eg. no argument) */
     if (isNaN(somevalue) === true && somevalue !== undefined )
       {
+        console.log('If not a number ',tmpArr);
         return tmpArr;
       }
 
@@ -15,11 +17,12 @@ module.exports = {
     and if there is no number specified, it creates a 3 element array */
     if (somevalue > 0)
       {
-        for(i =0; i < somevalue; i++)
+        for( i =0; i < somevalue; i++)
           {
             randomNumber = Math.floor(Math.random() *2);
             tmpArr.push(randomNumber);
           }
+        console.log('If specified argument ',tmpArr);
         return tmpArr;
       } else
         {
@@ -28,13 +31,43 @@ module.exports = {
               randomNumber = Math.floor(Math.random() *2);
               tmpArr.push(randomNumber);
             }
+          console.log('If no argument ',tmpArr);
           return tmpArr;
         }
 },
   
-  generate_2d : function()
-  {
+  generate_2d : function(first_arr_ele,second_arr_ele)
+ {
     var tmpArr = [];
+    var randomNumber;
+    var full_random = [];
+
+   /* if (isNaN(second_arr_ele) === true && second_arr_ele !== undefined )
+      {
+        return tmpArr;
+      }
+*/
+
+    //if (first_arr_ele === undefined && second_arr_ele === undefined)
+    //  {
+        for( i =0; i <  3; i++)
+          {
+            tmpArr.push(this.generate_1d());
+          }
+        console.log('full_random', tmpArr);
+        return tmpArr;
+    //  } 
+      /*else
+        {
+          for(var j =0; j < 3; j++)
+            {
+              randomNumber = this.generate_1d(second_arr_ele);
+              console.log('test2',randomNumber);
+              tmpArr.push(randomNumber);
+            }
+          return tmpArr;
+        }*/
+/*    var tmpArr = [];
     var innerArray = [];
       for(var i =0; i < 3; i++)
       {
@@ -46,7 +79,7 @@ module.exports = {
         }
       }
     tmpArr.push(innerArray);
-    return tmpArr;
+    return tmpArr;*/
   }
 
 };
--- test/test-generator.js
+++ test/test-generator.js
@@ -3,6 +3,7 @@ var expect = chai.expect;
 var should = chai.should();
 var mda = require('./../MultiDimensionalArray.js');
 
+// AN EXAMPLE
 /*describe('something', function() {
   it('should do something', function() {
     var something = 'some value';
@@ -18,7 +19,7 @@ describe('The generate_1d function should exist', function()
     it('and it should be a function huehuehue', function() 
       {
         expect(mda.generate_1d).to.exist();
-        expect(mda.generate_1d).to.be.an('function');
+        expect(mda.generate_1d).to.be.a('function');
       });
   });
 
@@ -26,24 +27,27 @@ describe('calling with no arguments', function()
   {
     it('should return an array with 3 random boolean values in it', function() 
       {
-        expect(mda.generate_1d()).to.be.an('array');
-        expect(mda.generate_1d()).to.have.length(3);
+        var result1 = mda.generate_1d();
+        expect(result1).to.be.an('array');
+        expect(result1).to.have.length(3);
       });
   });
 
 describe('calling with one argument 4', function(){
   it('should return an array with 4 random boolean values in it', function() 
   {
-    expect(mda.generate_1d(4)).to.be.an('array');
-    expect(mda.generate_1d(4)).to.have.length(4);
+    var result2 = mda.generate_1d(4);
+    expect(result2).to.be.an('array');
+    expect(result2).to.have.length(4);
   });
 });
 
 describe('calling with an argument "potato"', function(){
   it('should return an empty array', function() 
   {
-    expect(mda.generate_1d('potato')).to.be.an('array');
-    expect(mda.generate_1d('potato')).to.be.empty();
+    var result3 = mda.generate_1d('potato');
+    expect(result3).to.be.an('array');
+    expect(result3).to.be.empty();
   });
 });
 
@@ -66,37 +70,37 @@ describe('calling with an argument "potato"', function(){
   });
 });*/
 
-/*describe('The function generate_2d should exist', function()
+describe('The function generate_2d should exist', function()
   {
     it('should be a function', function() 
       {
         expect(mda.generate_2d).to.exist();
-        expect(mda.generate_2d).to.be.an('function');
+        expect(mda.generate_2d).to.be.a('function');
       });
   });
 
 describe('calling with no arguments', function()
   {
-    it('should return an array with 3 random boolean values in it', function() 
+    it('should return an array of 3 arrays with 3 random boolean values in each', function() 
       {
-        expect(mda.generate_1d()).to.be.an('array');
-        expect(mda.generate_1d()).to.have.length(3);
+        expect(mda.generate_2d()).to.be.an('array');
+        expect(mda.generate_2d()).to.have.length(3);
       });
   });
 
-describe('calling with one argument 4', function(){
-  it('should return an array with 4 random boolean values in it', function() 
+/*describe('calling with one argument 4', function(){
+  it('should return an array of 4 arrays with 3 random boolean values in each', function() 
   {
-    expect(mda.generate_1d(4)).to.be.an('array');
-    expect(mda.generate_1d(4)).to.have.length(4);
+    expect(mda.generate_2d(4)).to.be.an('array');
+    expect(mda.generate_2d(4)).to.have.length(4);
   });
 });
 
-describe('calling with an argument "potato"', function(){
-  it('should return an empty array', function() 
+describe('calling with arguments 5 and 4', function(){
+  it('should return an array of 5 arrays with 4 random booleans values in them', function() 
   {
-    expect(mda.generate_1d('potato')).to.be.an('array');
-    expect(mda.generate_1d('potato')).to.be.empty();
+    expect(mda.generate_2d(5,4)).to.be.an('array');
+    expect(mda.generate_2d(5,4)).to.have.length(5);
   });
 });
 */
