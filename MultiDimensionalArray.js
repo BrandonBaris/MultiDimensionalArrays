60dde44c1e47a2086c659f9cf8a8fdf2c9518f24
Brandon Baris <brandonbaris@gmail.com>
2015-01-22 10:07:05 -1000

    some comments

--- MultiDimensionalArray.js
+++ MultiDimensionalArray.js
@@ -2,13 +2,14 @@ module.exports = {
   generate_1d : function(somevalue)
   {
     var tmpArr = [];
+    var randomNumber;
     var i;
 
     /* tests if the value is not a number (eg. string) and if it's 
     not an undefined number (eg. no argument) */
     if (isNaN(somevalue) === true && somevalue !== undefined )
       {
-        console.log('If not a number ',tmpArr);
+        //console.log('If not a number ',tmpArr);
         return tmpArr;
       }
 
@@ -21,7 +22,7 @@ module.exports = {
             randomNumber = Math.floor(Math.random() *2);
             tmpArr.push(randomNumber);
           }
-        console.log('If specified argument ',tmpArr);
+        //console.log('If specified argument ',tmpArr);
         return tmpArr;
       } else
         {
@@ -30,7 +31,7 @@ module.exports = {
               randomNumber = Math.floor(Math.random() *2);
               tmpArr.push(randomNumber);
             }
-          console.log('If no argument ',tmpArr);
+          //console.log('If no argument ',tmpArr);
           return tmpArr;
         }
 },
@@ -43,6 +44,11 @@ module.exports = {
       {
         return tmpArr;
       } */
+
+
+    /* checks if there are 2 arguments, first_arr_ele and second_arr_ele.
+    second_arr_ele gets plugged into the function generate_1d then gets
+    looped by "first_arr_ele" amount of times */
 
     if (first_arr_ele > 0 && second_arr_ele > 0)
     {
@@ -82,34 +88,19 @@ module.exports = {
         return generate_2d(first_arr_ele,second_arr_ele);
       } */
 
+    /* If there are number values higher than 0 in the arguments then
+    it will plug second_arr_ele and third_arr_ele arguments into the
+    generate_2d function and loop that "first_arr_ele" amount of times*/
+    
     if (first_arr_ele > 0 && second_arr_ele > 0 && third_arr_ele > 0)
     {
       for(var  j =0; j < first_arr_ele; j++)
           {
             tmpArr.push(this.generate_2d(second_arr_ele,third_arr_ele));
           }
-        console.log('If 3 arguments are given ', tmpArr);
+        console.log(tmpArr);
 
         return tmpArr;
     }
-
-/*    if (first_arr_ele > 0 && second_arr_ele === undefined)
-    {
-      for( i =0; i <  first_arr_ele; i++)
-          {
-            tmpArr.push(this.generate_1d());
-          }
-        //console.log('If N arguements are given ', tmpArr);
-        return tmpArr;
-    } else
-    //if (first_arr_ele === undefined && second_arr_ele === undefined)
-      {
-        for( i =0; i <  3; i++)
-          {
-            tmpArr.push(this.generate_1d());
-          }
-        //console.log('If no arguments are given for anything ', tmpArr);
-        return tmpArr;
-    } */
   }
 };
